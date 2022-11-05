0989b22f4eabc94bd00d3c9fe4405bf96c06d8b8
pgmatev <petargabriel.matev@gmail.com>
2022-11-05 16:20:31 +0200

    stack implementation

---
+++ lectures/week05/stack.cpp
@@ -0,0 +1,72 @@
+#include "stack.hh"
+
+#include <cassert>
+
+template <class T>
+Stack<T>::Stack() : count(0), head(nullptr)
+{
+
+}
+
+template <class T>
+Stack<T>::Stack(const Stack<T>& other)
+{
+    head = copy(other.head);
+}
+
+template <class T>
+Stack<T>::~Stack()
+{
+    
+}
+
+template <class T>
+Stack<T>& Stack<T>::operator=(const Stack<T>& other)
+{
+    if (*this != other)
+    {
+        
+    }
+}
+
+template <class T>
+void Stack<T>::push(const T& x)
+{
+    head = new Stack<T>::box{x, head};
+    count++;
+}
+
+template <class T>
+void Stack<T>::pop()
+{
+    assert(head != nullptr);
+    Stack<T>::box* head_cpy = head;
+    head = head->next;
+    delete head_cpy;
+    count--;
+}
+
+template <class T>
+T Stack<T>::top()
+{
+    return head->data;
+}
+
+template <class T>
+size_t Stack<T>::size()
+{
+    return count;
+}
+
+
+template <class T>
+typename Stack<T>::box* Stack<T>::copy(typename Stack<T>::box *other_head)
+{
+    if (other_head == nullptr)
+    {
+        return nullptr;
+    }
+    return new Stack<T>::box {other_head->data, copy(head->next)};
+}
+
+
---
+++ lectures/week05/stack.hh
@@ -0,0 +1,30 @@
+#ifndef STACK_HH
+#define STACK_HH
+
+template <class T>
+class Stack
+{
+    private:
+    struct box
+    {
+        T data;
+        box *next;
+    };
+    box* head;
+    size_t count;
+    public:
+    Stack();
+    Stack(const Stack&);
+    ~Stack();
+    Stack& operator=(const Stack&);
+
+    T top();
+    void pop();
+    void push(const T&);
+    bool empty();
+    size_t size();
+    box* copy(box*);
+
+};
+
+#endif
